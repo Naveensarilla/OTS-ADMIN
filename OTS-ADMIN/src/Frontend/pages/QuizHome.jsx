30e26aea402b8e3cbf963a9018a85cfa21e35428
AHarshitha04 <harshitha.a@egradtutor.in>
2023-12-03 10:51:02 +0530

    ...

---
+++ OTS-ADMIN/src/Frontend/pages/QuizHome.jsx
@@ -0,0 +1,14 @@
+import "./QuizHome.css";
+import SubjectTest from "../SubjectTest/SubjectTest";
+
+const QuizHome = () => {
+  return (
+    <div className="home-page">
+      <div>
+        <SubjectTest />
+      </div>
+    </div>
+  );
+};
+
+export default QuizHome;
